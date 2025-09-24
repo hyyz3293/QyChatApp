1ed40dc04a8cbee9c7eb25c25cb6a9afeeaca211
hyyz3293 <377956638@qq.com>
2025-09-24 15:10:37 +0800

    1
    1

--- lib/presentation/utils/websocket/chat_socket_manager.dart
+++ lib/presentation/utils/websocket/chat_socket_manager.dart
@@ -677,15 +677,15 @@ class CSocketIOManager {
             eventBus.fire(NoOnlineServiceEvent(true));
           } else {
             eventBus.fire(NoOnlineServiceEvent(false));
-            msgId = messId ?? "";
-            // 处理welcomeSpeech可能为null的情况
-            var message = Message(
-                createdAt: dateTime,
-                status: MessageStatus.delivered,
-                message: "$msg",
-                sentBy: '$userId'
-            );
-            _sendMessage(message);
+            // msgId = messId ?? "";
+            // // 处理welcomeSpeech可能为null的情况
+            // var message = Message(
+            //     createdAt: dateTime,
+            //     status: MessageStatus.delivered,
+            //     message: "$msg",
+            //     sentBy: '$userId'
+            // );
+            // _sendMessage(message);
           }
         } catch (e) {
           print('解析 imSeatReturnResult 失败: $e');
